7a7349b6cc1d7dffc464353ec8a46a98f844eda8
Jacken <jacken1013@gmail.com>
2015-07-09 00:30:38 +0800

    * [common] First upload.

---
+++ common/Log.cpp
@@ -0,0 +1,13 @@
+/*
+ * Log.cpp
+ *
+ *  Created on: Jul 9, 2015
+ *      Author: jacken
+ */
+
+#include "Log.h"
+
+bool LogUtil::Print()
+{
+	return true;
+}
---
+++ common/Log.h
@@ -0,0 +1,18 @@
+/*
+ * Log.h
+ *
+ *  Created on: Jul 9, 2015
+ *      Author: jacken
+ */
+
+#ifndef LOG_H_
+#define LOG_H_
+
+class LogUtil
+{
+public:
+	static bool Print();
+};
+
+
+#endif /* LOG_H_ */
