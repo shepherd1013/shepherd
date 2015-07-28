0aa0f21bbb843d5cdcfad418d00642e2f9afdee5
Jacken <jacken1013@gmail.com>
2015-07-28 17:31:55 +0800

    * [common] Add INF_PRINT macro and print filename.

--- common/debug.h
+++ common/debug.h
@@ -18,14 +18,18 @@
 #define DEBUG_OUTPUT stderr
 
 #define DBG_PRINT(fmt, args ...) {\
-	fprintf(DEBUG_OUTPUT, "[DBG] %s:%d:	", __FUNCTION__, __LINE__);\
+	fprintf(DEBUG_OUTPUT, "[DBG] [%s:%s:%d] ", __FILE__, __FUNCTION__, __LINE__);\
 	fprintf(DEBUG_OUTPUT, fmt, ## args);}
 #else
 #define DBG_PRINT(message, arsg ...)
 #endif
 
+#define INF_PRINT(fmt, args ...) {\
+	fprintf(stderr, "[INF] [%s:%s:%d] ", __FILE__, __FUNCTION__, __LINE__);\
+	fprintf(stderr, fmt, ## args);}
+
 #define ERR_PRINT(fmt, args ...) {\
-	fprintf(stderr, "[ERR] %s:%d:	", __FUNCTION__, __LINE__);\
+	fprintf(stderr, "[ERR] [%s:%s:%d] ", __FILE__, __FUNCTION__, __LINE__);\
 	fprintf(stderr, fmt, ## args);}
 
 #endif /* DEBUG_H_ */
