1d94438a73af23064022839bce304db7d4934b61
Jacken <jacken1013@gmail.com>
2015-09-20 16:26:59 +0800

    * [common_unittest] First upload.

---
+++ common_unittest/common_unittest.h
@@ -0,0 +1,21 @@
+/*
+ * common_unittest.h
+ *
+ *  Created on: Sep 20, 2015
+ *      Author: jacken
+ */
+
+#ifndef COMMON_UNITTEST_H_
+#define COMMON_UNITTEST_H_
+
+class Unittest
+{
+public:
+	void Run();
+private:
+	bool TestFileParser();
+};
+
+
+
+#endif /* COMMON_UNITTEST_H_ */
