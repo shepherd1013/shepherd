9d4411b461ab04b28c79a27af54dc08bec09e200
Jacken <jacken1013@gmail.com>
2015-09-20 15:46:30 +0800

    * [common] First upload.

---
+++ common/common_define.h
@@ -0,0 +1,18 @@
+/*
+ * common_define.h
+ *
+ *  Created on: Sep 18, 2015
+ *      Author: jacken
+ */
+
+#ifndef COMMON_DEFINE_H_
+#define COMMON_DEFINE_H_
+
+#ifndef BOOL
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+#endif
+
+
+#endif /* COMMON_DEFINE_H_ */
