36b7371b91b645569ec3a3405c4e5140f62eb7ee
Jacken <jacken1013@gmail.com>
2016-02-18 10:54:29 +0800

    * [common] Implement command parser.

---
+++ common/spd_cmd_parser.cpp
@@ -0,0 +1,56 @@
+/*
+ * spd_cmd_parser.cpp
+ *
+ *  Created on: Feb 17, 2016
+ *      Author: Jacken
+ */
+#include <string.h>
+#include "debug.h"
+#include "spd_cmd_parser.h"
+
+CommandParser::CommandParser()
+{
+}
+
+CommandParser::CommandParser(const char* sData)
+{
+	this->LoadData(sData);
+}
+
+bool CommandParser::LoadData(const char* sData)
+{
+	if (sData == NULL) {
+		ERR_PRINT("The data is NULL!\n");
+		return false;
+	}
+	CmdMap.clear();
+	strncpy(sBuf, sData, sizeof(sBuf));
+	char *pSave1;
+	char *pSave2;
+	const char* sKey;
+	const char* sVal;
+	char *pStr1 = strtok_r(sBuf, "&", &pSave1);
+	char *pStr2;
+	while (pStr1 != NULL) {
+		pStr2 = strtok_r(pStr1, "=", &pSave2);
+		sKey = pStr2;
+		pStr2 = strtok_r(NULL, "=", &pSave2);
+		sVal = pStr2;
+		CmdMap.insert(pair<string, string>(sKey, sVal));
+		pStr1 = strtok_r(NULL, "&", &pSave1);
+	}
+	return true;
+}
+
+const char* CommandParser::GetKeyValue(const char *sKey)
+{
+	if (sKey == NULL) {
+		ERR_PRINT("The key is NULL!\n");
+		return NULL;
+	}
+	map<string, string>::iterator it = CmdMap.find(sKey);
+	if (it == CmdMap.end()) {
+		return NULL;
+	}
+	return it->second.c_str();
+}
---
+++ common/spd_cmd_parser.h
@@ -0,0 +1,30 @@
+/*
+ * spd_cmd_parser.h
+ *
+ *  Created on: Feb 17, 2016
+ *      Author: Jacken
+ */
+
+#ifndef SPD_CMD_PARSER_H_
+#define SPD_CMD_PARSER_H_
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+class CommandParser
+{
+public:
+	CommandParser();
+	CommandParser(const char* sData);
+	const char* GetKeyValue(const char *sKey);
+	bool LoadData(const char* sData);
+
+private:
+	char sBuf[4096];
+	map<string, string> CmdMap;
+};
+
+
+#endif /* SPD_CMD_PARSER_H_ */
